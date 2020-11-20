2f465a0718fe5880e9ac2d49de61cf7c6b39b380
Sindri P <sindripetur@gmail.com>
2020-11-20 23:12:04 +0100

    Adds an example dotfile that is linked into .config

--- home/default.nix
+++ home/default.nix
@@ -5,15 +5,17 @@
 
   home.packages = [
     #pkgs.tmux
+    pkgs.home-manager
     pkgs.direnv
     pkgs.rust-analyzer
   ];
 
+  programs.tmux.enable = true;
 
   #xdg.enable = true;
-  #xdg.configFile = {
-  #  "ideavim/ideavimrc".source = "./dotconfig/ideavim/ideavimrc";
-  #};
+  xdg.configFile = {
+    "ideavim/ideavimrc".source = ./dotconfig/ideavim/ideavimrc;
+  };
 
 }
 
