d8e33a18c7f5103e9b990db7c0b733e9c87e1784
Sindri P <sindripetur@gmail.com>
2020-12-09 00:48:40 +0100

    Update

--- home/default.nix
+++ home/default.nix
@@ -4,15 +4,29 @@
   home.stateVersion = "20.09";
 
   home.packages = [
-    #pkgs.tmux
     pkgs.home-manager
+    pkgs.cachix
+
+    # Terminal
+    pkgs.git
+    pkgs.starship
+    pkgs.tmux
     pkgs.direnv
+    pkgs.fzf
+    pkgs.bat
+    pkgs.gitAndTools.delta
+
+    # LSP
     pkgs.rust-analyzer
+    pkgs.texlab
+
+    #Other
+    pkgs.zotero
+    pkgs.zathura
   ];
 
   programs.tmux.enable = true;
 
-  #xdg.enable = true;
   xdg.configFile = {
     "ideavim/ideavimrc".source = ./dotconfig/ideavim/ideavimrc;
     "nix/nix.conf".source = ./dotconfig/nix/nix.conf;
